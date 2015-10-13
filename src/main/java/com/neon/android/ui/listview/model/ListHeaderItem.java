294318a45a3b4d10d4ae6d19d1de0c21abb87c55
dsneves <dsneves@impresa.pt>
2015-10-13 17:29:37 +0100

    expanded info

--- src/main/java/com/neon/android/ui/listview/model/ListHeaderItem.java
+++ src/main/java/com/neon/android/ui/listview/model/ListHeaderItem.java
@@ -4,8 +4,18 @@ import com.neon.android.ui.listview.RowItemHolder;
 
 public class ListHeaderItem< TYPE, VIEW_HOLDER > extends ListItem< TYPE, VIEW_HOLDER > {
 
+    private boolean expanded = true;
+
     public ListHeaderItem( RowItemHolder<TYPE, VIEW_HOLDER > viewHolder ) {
         super(viewHolder);
+    }
+
+    public void setExpanded(boolean expanded) {
+        this.expanded = expanded;
+    }
+
+    public boolean isExpanded() {
+        return expanded;
     }
 
 }
